8732022f712f07ee38694367b01e9f1d27cc323e
idkvaiop <104100121+idkvaiop@users.noreply.github.com>
2022-06-06 10:13:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 from telebot.types import InlineKeyboardButton
 
 # Fillout Here The BotToken it gets from botfather further queries @riz4d 0n telegram
-bot = telebot.AKJAS('BOT_TOKEN')
+bot = telebot.TeleBot('BOT_TOKEN')
 
 while True:
     try:
